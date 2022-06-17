e64b0b4f2c19cfd9f4afe2dacba582354556a638
Jordi Lucas <jordiluc16@gmail.com>
2022-06-17 16:00:39 +0200

    Refactoring get_api_data function

--- main.py
+++ main.py
@@ -4,21 +4,19 @@ from utils.utils import get_data_x_page, get_api_key, API_FOOTBALL_PLAYERS_ENDPO
     get_total_pages
 
 
-def collect_api_data():
+def get_api_data():
 
     # Get API Key
     key = get_api_key()
-    # Get total pages
-    total_pages = get_total_pages()
-    # Get Liga dataset
-    df_liga = get_data_x_page(1, total_pages, API_FOOTBALL_PLAYERS_ENDPOINT, key)
+    # Get championship dataset
+    df_championship = get_data_x_page(1, get_total_pages(), API_FOOTBALL_PLAYERS_ENDPOINT, key)
     # Get date now
     datetime_now = datetime.datetime.now().strftime("%Y-%m-%d%H:%M:%S")
-    # Save La Liga dataset in csv format
-    df_liga.to_csv('data/data_liga_' + datetime_now + '.csv', index=False, header=True)
+    # Save championship dataset in csv format
+    df_championship.to_csv('data/df_championship_' + datetime_now + '.csv', index=False, header=True)
 
 
-# Press the green button in the gutter to run the script.
+# The main one.
 if __name__ == '__main__':
-    collect_api_data()
+    get_api_data()
 
