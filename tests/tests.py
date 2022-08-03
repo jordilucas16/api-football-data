73a80c3f0fd1c5bbc3d0f2b90b0767eb7ed434ad
Jordi Lucas <jordiluc16@gmail.com>
2022-08-03 08:16:17 +0200

    Add test_get_total_pages test

--- tests/tests.py
+++ tests/tests.py
@@ -1,6 +1,6 @@
 import unittest
 
-from utils.utils import get_api_response, get_api_key, PREMIER_LEAGUE, SEASON, TOO_MANY_REQUESTS
+from utils.utils import get_api_response, get_api_key, PREMIER_LEAGUE, SEASON, TOO_MANY_REQUESTS, get_total_pages
 
 
 class Test_API_football(unittest.TestCase):
@@ -13,6 +13,7 @@ class Test_API_football(unittest.TestCase):
         # Put your own API Key here ;-)
         cls._key = get_api_key()
         cls._response = get_api_response(cls._url, cls._qs, cls._key)
+        cls._total_pages = get_total_pages()
 
     def test_get_api_football_response(self):
         print("Get API Football response Test")
@@ -22,3 +23,10 @@ class Test_API_football(unittest.TestCase):
         self.assertNotEqual(self._response.status_code, 404)
         self.assertNotEqual(self._response.status_code, 500)
         self.assertNotEqual(self._response.status_code, TOO_MANY_REQUESTS)
+
+    def test_get_total_pages(self):
+        print("Get total pages Test")
+        self.assertIsInstance(self._total_pages, int)
+        self.assertNotIsInstance(self._total_pages, list)
+        self.assertNotIsInstance(self._total_pages, tuple)
+        self.assertNotIsInstance(self._total_pages, dict)
